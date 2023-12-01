e79193c5cad0d2de59959b20ed854da0a15bf4dc
AlessandraRomualdo <romualdo.alessandra23@gmail.com>
2023-12-01 14:44:07 -0300

    feat: controller & view POST

--- src/controllers/traduzo_controller.py
+++ src/controllers/traduzo_controller.py
@@ -1,23 +1,36 @@
 from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
+
 
 traduzo_controller = Blueprint("traduzo_controller", __name__)
 
 
-@traduzo_controller.route("/", methods=["GET"])
+@traduzo_controller.route("/", methods=["GET", "POST"])
 def index():
-    languages = LanguageModel.list_dicts()
-    text_to_translate = request.form.get(
-        'text-to-translate') or 'O que deseja traduzir?'
-    translate_from = request.form.get('translate-from')
-    translate_to = request.form.get('translate-to')
-    translated = request.form.get(
-        'translated') or 'What do you want to translate?'
-    return render_template(
-        "index.html",
-        languages=languages,
-        text_to_translate=text_to_translate,
-        translate_from=translate_from,
-        translate_to=translate_to,
-        translated=translated,
-    )
+    if request.method == "POST":
+        text_to_translate = request.form.get("text-to-translate")
+        translate_from = request.form.get("translate-from")
+        translate_to = request.form.get("translate-to")
+
+        translated = GoogleTranslator(
+            source=translate_from, target=translate_to
+            ).translate(text_to_translate)
+
+        return render_template(
+            "index.html",
+            languages=LanguageModel.list_dicts(),
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated,
+        )
+    else:
+        return render_template(
+            "index.html",
+            languages=LanguageModel.list_dicts(),
+            text_to_translate="O que deseja traduzir?",
+            translate_from="pt",
+            translate_to="en",
+            translated="What do you want to translate?"
+        )
